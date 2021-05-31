dd8f08b7db88bba2d1b56a0317779bb1d93bd6fe
Adrien Goeller <adrien.goeller.pro@gmail.com>
2021-05-31 22:12:25 +0200

    bug for building

--- setup.py
+++ setup.py
@@ -1,14 +1,41 @@
+import sys
+from pathlib import Path
+from typing import List
+
+projet_dir: Path = Path(__file__).resolve().parent
+source_dir: Path = projet_dir / "src"
+sys.path.insert(0,str(source_dir))
+
+from sensorsim import LICENCE, AUTHOR, AUTHOR_EMAIL, PROJECT, DESCRIPTION, __version__ as VERSION
+
 from setuptools import setup, find_packages
 
-setup(
-     name='sensorsim',
-     version='0.0.1',
-     author='Adrien Goeller',
-     author_email='adrien.goeller.pro@gmail.com',
-     packages=find_packages('./src/sensorsim'),
-     url='http://pypi.python.org/pypi/PackageName/', 
-     python_requires='>=3',
-     license='LICENSE', 
-     description='Function collection for specific pressure sensor simulation',
-     long_description=open('README.md').read(),
-    )
+
+def parse_requirements(filename: str) -> List[str]:
+    """Load requirements from pip requirements file"""
+    with open(filename, "r") as f:
+        lines: List[str] = []
+        for l in f:
+            l.strip()
+            if l and not l.startswith('#'):
+                lines.append(l)
+    return lines
+
+requirements: List[str] = parse_requirements("requirements.txt")
+
+if __name__ == "__main__":
+    setup(
+        name=PROJECT,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        keywords=['sensorsim'],
+        packages=find_packages(source_dir),
+        package_dir= {"sensorsim":source_dir / "sensorsim"},
+        url='https://github.com/adriengoeller/python-sensor-simulation', 
+        python_requires='>=3',
+        license='LICENSE', 
+        description=DESCRIPTION,
+        long_description=open('README.md').read(),
+        )
+    
--- src/sensorsim/__init__.py
+++ src/sensorsim/__init__.py
@@ -1,9 +1,22 @@
-PROJECT = "sensorsim"
-NAME = "sensorsim"
-VERSION = 0.0.1
+from sensorsim import instruments, montages
+
+
 
 
 __all__ = [
     'instruments',
-    'montage'
+    'montages'
 ]
+
+#: Project's name
+
+PROJECT:str = "sensorsim"
+NAME:str = "sensorsim"
+VERSION:str = "0.0.1"
+DESCRIPTION:str = "Function collection for specific pressure sensor simulation"
+AUTHOR: str = "Adrien Goeller"
+AUTHOR_EMAIL: str = "https://github.com/adriengoeller"
+VERSION : str = "0.0.1"
+__version__:str = VERSION
+COPYRIGHT: str = "LICENSE"
+LICENCE: str = "LICENSE"
